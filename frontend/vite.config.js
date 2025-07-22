78cb02ee66bd1473c62bfeaed301916a98a87be6
Loylie <fspuntnl@gmail.com>
2025-07-22 19:06:39 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
   server: {
     proxy: {
       '/api': {
