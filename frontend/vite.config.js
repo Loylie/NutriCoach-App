2e6e4ec0698cf5745f275cfd17d056733c22b2a6
Loylie <fspuntnl@gmail.com>
2025-07-22 18:08:18 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins: [react( )],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/.netlify/functions',
+        changeOrigin: true,
+        rewrite: (path ) => path.replace(/^\/api/, ''),
+      },
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
